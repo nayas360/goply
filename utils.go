2d373e393cd06ee22c984e15f1417e612c4b1615
Sayan Dutta <duttasayan.7@gmail.com>
2017-12-23 17:31:54 +0530

    Clean up
    > removed parser related code from 4f246f768542ad95283f8f4598138a5e0ecfc489

--- utils.go
+++ utils.go
@@ -10,23 +10,8 @@ func computeSha1(text string) string {
 	return fmt.Sprintf("%x", sha1.Sum([]byte(text)))
 }
 
-// checks if the given token is a terminal token
-func isTerminalType(Type string) bool {
-	return len(Type) > len(plx_TERMINAL_B) && plx_TERMINAL_B == Type[:len(plx_TERMINAL_B)]
-}
-
 // The default error handler
 func defaultLexerError(ls LexerState) error {
 	return fmt.Errorf("line %d, column %d: could not match '%c' with any rule", ls.LineNum, ls.ColNum,
 		ls.Source[ls.Position])
-}
-
-// The parsers default error handler for lexer
-func plxDefaultErrorFunc(ls LexerState) error {
-	symbol := string(ls.Source[ls.Position])
-	if symbol == "\n" {
-		symbol = "\\n"
-	}
-	return fmt.Errorf("unexpected symbol '%s' at position '%d' in '%s'", symbol,
-		ls.Position, ls.Source)
 }
